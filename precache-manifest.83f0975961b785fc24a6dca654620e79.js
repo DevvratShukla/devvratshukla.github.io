80b5e177a803c7948f442b65a5ff45c79f8069db
Devvrat Shukla <dshukla@ptc.com>
2018-12-23 23:36:04 +0530

    feat: New base for the react based portfolio

---
+++ precache-manifest.83f0975961b785fc24a6dca654620e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a0ff2e5db86a4c0f7e4728df6d0ff3",
+    "url": "/static/media/devvrat-profile-pic.02a0ff2e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10b2a191e2b2597bb765",
+    "url": "/static/js/main.10b2a191.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "10b2a191e2b2597bb765",
+    "url": "/static/css/main.ca31f034.chunk.css"
+  },
+  {
+    "revision": "d95f0f98202e781de859788117226675",
+    "url": "/index.html"
+  }
+];

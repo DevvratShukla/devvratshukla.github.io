165522fe40c9d6d422768c170fe81f5f4fbaffa2
Devvrat Shukla <dshukla@ptc.com>
2019-05-28 00:21:14 +0530

    Updates

---
+++ precache-manifest.1c81a6c260f28b88e7513b1a293fa297.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fed43474de39b10c952be9c45295be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04837c6ccf4a0d67c939",
+    "url": "/static/css/2.4153d5dc.chunk.css"
+  },
+  {
+    "revision": "95ed7a18984f9ea40c23",
+    "url": "/static/css/main.2168c0ca.chunk.css"
+  },
+  {
+    "revision": "04837c6ccf4a0d67c939",
+    "url": "/static/js/2.93f349b7.chunk.js"
+  },
+  {
+    "revision": "95ed7a18984f9ea40c23",
+    "url": "/static/js/main.6dce1c05.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d23373896171d412ed8b8a6a0b752864",
+    "url": "/static/media/devvrat-profile-pic.d2337389.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
